6c0c6996879d8a3897be4481d9e38bf5f3ac29d1
MaksimK30 <makskuzmih@mail.ru>
2024-01-17 01:30:09 +0300

    Изменён контекст

--- Context/HotelDbContext.cs
+++ Context/HotelDbContext.cs
@@ -76,7 +76,7 @@ public partial class HotelDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("host=localhost;Port=5432;Username=postgres;Password=8567;Database=HotelDB");
+        => optionsBuilder.UseLazyLoadingProxies().UseNpgsql("host=localhost;Port=5432;Username=postgres;Password=8567;Database=HotelDB");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
