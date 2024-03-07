847fd01985d9d2e38820761f6ee3b4255fd442dd
MaksimK30 <makskuzmih@mail.ru>
2024-03-07 08:25:39 +0300

    Подготовлена вкладка Rooms

--- ViewModels/Tabs/FinancialAccounting/RoomsTabVM.cs
+++ ViewModels/Tabs/FinancialAccounting/RoomsTabVM.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading;
 using HotelDesktop.Context;
 using HotelDesktop.Models;
 using HotelDesktop.Models.ExtendedModels;
@@ -11,8 +12,6 @@ namespace HotelDesktop.ViewModels.Tabs;
 public class RoomsTabVM : ViewModelBase
 {
     private ObservableCollection<RoomsTabRecord> _roomsList;
-    private int _selectedSearchParameter;
-    private List<string> _searchParameters;
     private int _selectedRoomType;
     private List<string> _roomTypeList;
     private bool _minibarCB;
@@ -36,18 +35,6 @@ public class RoomsTabVM : ViewModelBase
         set { this.RaiseAndSetIfChanged(ref _roomsList, value); }
     }
 
-    public int SelectedSearchParameter
-    {
-        get { return _selectedSearchParameter; }
-        set { this.RaiseAndSetIfChanged(ref _selectedSearchParameter, value); }
-    }
-
-    public List<string> SearchParameters
-    {
-        get { return _searchParameters; }
-        set { this.RaiseAndSetIfChanged(ref _searchParameters, value); }
-    }
-
     public int SelectedRoomType
     {
         get { return _selectedRoomType; }
@@ -65,23 +52,27 @@ public class RoomsTabVM : ViewModelBase
         HotelDbContext dbContext = new HotelDbContext();
         List<Room> allRooms = dbContext.Rooms.ToList();
         List<RoomsTabRecord> roomsList = new List<RoomsTabRecord>();
+        RoomTypesList = new List<string>();
 
         foreach (var room in allRooms)
         {
             roomsList.Add(new RoomsTabRecord()
             {
-
+                Rooms = room.Rooms,
+                Cost = room.Cost,
+                Floor = room.Floor,
+                Jacuzzi = room.Jacuzzi,
+                Minibar = room.Minibar,
+                Number = room.Number,
+                RoomType = room.RoomType.Title
             });
         }
-
+        
+        RoomTypesList.Add("Все типы");
+        RoomTypesList.AddRange(dbContext.RoomTypes.Select(r => r.Title).ToList());
+        SelectedRoomType = 0;
+        
+        
         RoomsList = new ObservableCollection<RoomsTabRecord>(roomsList);
-
-        SearchParameters = new List<string>()
-        {
-            "Номер",
-            "Этаж"
-        };
-
-        SelectedSearchParameter = 0;
     }
 }
