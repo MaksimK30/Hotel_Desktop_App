92d5ac6f7aba279e0b68d0ecbc8dd606266b67e6
MaksimK30 <makskuzmih@mail.ru>
2024-03-09 21:58:12 +0300

    Создан список платёжные системы

--- ViewModels/Tabs/PlacementManagement/PaymentSystemsTabVM.cs
+++ ViewModels/Tabs/PlacementManagement/PaymentSystemsTabVM.cs
@@ -1,6 +1,39 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using HotelDesktop.Context;
+using HotelDesktop.Models;
+using HotelDesktop.Models.ExtendedModels;
+using ReactiveUI;
+
 namespace HotelDesktop.ViewModels.Tabs;
 
 public class PaymentSystemsTabVM : ViewModelBase
 {
-    
+    private List<PaymentSystemsTabRecord> _paymentSystemsList;
+    private int _selectedSearchParameter;
+    private List<string> _searchParameters;
+
+    public List<PaymentSystemsTabRecord> PaymentSystemsList
+    {
+        get { return _paymentSystemsList; }
+        set { this.RaiseAndSetIfChanged(ref _paymentSystemsList, value); }
+    }
+
+    public PaymentSystemsTabVM()
+    {
+        HotelDbContext dbContext = new HotelDbContext();
+        List<PaymentSystem> allPaymentSystems = dbContext.PaymentSystems.ToList();
+        List<PaymentSystemsTabRecord> paymentSystemsList = new List<PaymentSystemsTabRecord>();
+
+        foreach (var system in allPaymentSystems)
+        {
+            paymentSystemsList.Add(new PaymentSystemsTabRecord()
+            {
+                Title = system.Title
+            });
+        }
+
+        PaymentSystemsList = paymentSystemsList;
+    }
 }
